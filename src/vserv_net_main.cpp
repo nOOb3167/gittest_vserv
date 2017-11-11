a0cec534d7c469a4c6f19699d380d0fa2efcb03d
nOOb3167 <nOOb3167@gmail.com>
2017-11-11 08:15:53 +0100

    gittest_vserv: indentation

--- src/vserv_net_main.cpp
+++ src/vserv_net_main.cpp
@@ -13,37 +13,37 @@ GsLogList *g_gs_log_list_global = gs_log_list_global_create();
 
 int main(int argc, char **argv)
 {
-  int r = 0;
+	int r = 0;
 
-  struct GsConfMap *ConfMap = NULL;
-  struct GsAuxConfigCommonVars CommonVars = {};
+	struct GsConfMap *ConfMap = NULL;
+	struct GsAuxConfigCommonVars CommonVars = {};
 
-  if (!!(r = gs_log_crash_handler_setup()))
-    GS_GOTO_CLEAN();
+	if (!!(r = gs_log_crash_handler_setup()))
+		GS_GOTO_CLEAN();
 
-  if (!!(r = gs_config_read_default_everything(&ConfMap)))
-    GS_GOTO_CLEAN();
+	if (!!(r = gs_config_read_default_everything(&ConfMap)))
+		GS_GOTO_CLEAN();
 
-  if (!!(r = gs_config_get_common_vars(ConfMap, &CommonVars)))
-    GS_GOTO_CLEAN();
+	if (!!(r = gs_config_get_common_vars(ConfMap, &CommonVars)))
+		GS_GOTO_CLEAN();
 
-  if (!!(r = gs_config_create_common_logs(ConfMap)))
-    GS_GOTO_CLEAN();
+	if (!!(r = gs_config_create_common_logs(ConfMap)))
+		GS_GOTO_CLEAN();
 
-  {
-    log_guard_t Log(GS_LOG_GET("serv"));
+	{
+		log_guard_t Log(GS_LOG_GET("serv"));
 
-    if (!!(r = gs_vserv_start_crank0(&CommonVars)))
-      GS_GOTO_CLEAN();
-    }
+		if (!!(r = gs_vserv_start_crank0(&CommonVars)))
+			GS_GOTO_CLEAN();
+	}
 
 clean:
-  GS_DELETE_F(&ConfMap, gs_conf_map_destroy);
+	GS_DELETE_F(&ConfMap, gs_conf_map_destroy);
 
-  gs_log_crash_handler_dump_global_log_list_suffix("_log", strlen("_log"));
+	gs_log_crash_handler_dump_global_log_list_suffix("_log", strlen("_log"));
 
-  if (!!r)
-    return EXIT_FAILURE;
+	if (!!r)
+		return EXIT_FAILURE;
 
-  return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
